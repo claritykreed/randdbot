859e5eaa20a1fc1f930d4ea9d01478263c8f9de0
Kreed <derekandrews@protonmail.com>
2017-09-29 14:26:44 +0000

    Procfile for Heroku

--- config.js
+++ config.js
@@ -6,21 +6,3 @@ module.exports = {
   access_token: process.env.BOT_ACCESS_TOKEN,
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 };
-
-//Access codes for Budget2018 twitter page
-
-// module.exports = {
-//   consumer_key: 'ZWISGW0wxMeH281Rv7dCs1NrB',
-//   consumer_secret: 'Frrsojh2gzeKUqSZHzhIBkDzJ8SfsVkjzlj2lUHianaxnBmdki',
-//   access_token: '	902291241337053187-daYLWQtKQ7DtyXGN25ZCJeqbQaRs9Lr',
-//   access_token_secret: 'tpGr4KDhWlxyQiIlS53fVPNHCbvyJMPRPeWVI1uZZUGNQ'
-// };
-
-//Access codes for Demotaxtwit
-
-// module.exports = {
-//   consumer_key: 'HsgUcNRRuJDnpff4WnMq555tf',
-//   consumer_secret: 'IKR7jQjMbN3EmGtHeUJBxP5tQ40Siu0RQRdknTbe4jHd4gAxfA',
-//   access_token: '904453598570061824-w0QWYv3FnyVjeozNTfu648Sfn2IhJ9g',
-//   access_token_secret: 'dxsiiauHdWZNOuCJaScIvqtKKEXfpNpElq8O9XxPT4uk3'
-// };
